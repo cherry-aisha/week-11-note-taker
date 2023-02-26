befa44fa293efd48a5491d23bfc9eeb66e12bb9a
cherry-aisha <113786104+cherry-aisha@users.noreply.github.com>
2023-02-26 11:28:25 +0000

    Updated apiroutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,16 +2,40 @@ const router = require ('express').Router();
 const save = require('../db/db.json');
 
 //Get request for notes
-router.get('/notes', (req, res) => { save
-    .getNotes()
+
+// Add code which opens uo the file and writes it into the array,
+// exercises at the back end of the week. Append, read, write...
+// 24 feedback.js example fsUtils exampls.
+router.get('/notes', (req, res) => { save ()
       .then((notes) => { return res.json(notes); })
       .catch((err => res.status(500).json(err)));
+      readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+      .then((json) => {
+        const result = json.filter((note) => note.note_title === note);
+        return result.length > 0
+          ? res.json(result)
+          : res.json('No note');
+      });
 });
 
 router.post('/notes', (req, res) => { save
     .addNote(req.body)
     .then((note) => res.json(note))
     .catch((err) => res.status(500).json(err));
+
+  const { username, topic, tip } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title,
+      text
+    };
+
+    readAndAppend(newNote, './db/db.json');
+    res.json(`Note added successfully 🚀`);
+  } else {
+    res.error('Error in adding note');
+  }
 });
 
 //Delete notes
