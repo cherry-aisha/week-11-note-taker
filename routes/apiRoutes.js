e1026dcef4ed61e8f756c3fd66ab7d1ea6a3fb4a
cherry-aisha <113786104+cherry-aisha@users.noreply.github.com>
2023-02-26 13:40:30 +0000

    Updated apiroutes and utils

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,31 +1,27 @@
 const note = require('express').Router();
-const { readAndAppend } = require('../helpers/fsUtils');
+const { readAndAppend, readFromFile, writeToFile } = require('../helpers/fsUtils');
 const uuid = require('../helpers/uuid');
 
 // GET Route for retrieving all the feedback
-note.get('/', (req, res) =>
+note.get('/notes/', (req, res) =>
   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
 );
 
 // POST Route for submitting feedback
-note.post('/', (req, res) => {
+note.post('/notes/', (req, res) => {
   // Destructuring assignment for the items in req.body
   const { title, text } = req.body;
 
   // If all the required properties are present
   if (title && text) {
     // Variable for the object we will save
-    const newFeedback = {
+    const newNote = {
       title,
       text,
-      note_id: uuid(),
+      id: uuid(),
     };
 
     readAndAppend(newNote, './db/db.json');
-    const response = {
-      status: 'success',
-      body: newNote,
-    };
 
     res.json(newNote);
   } else {
@@ -33,78 +29,22 @@ note.post('/', (req, res) => {
   }
 });
 
+note.delete('/notes/:id', (req, res) => {
+
+  const noteId = req.params.id;
+  readFromFile('./db/db.json')
+
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      // Make a new array of all notes except the one with the ID provided in the URL
+      const result = json.filter((note) => note.id !== noteId);
+
+      // Save that array to the filesystem
+      writeToFile('./db/db.json', result);
+
+      // Respond to the DELETE request
+      res.json(`Item ${noteId} has been deleted 🗑️`);
+    });
+});
+
 module.exports = note;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-const router = require ('express').Router();
-const save = require('../db/db.json');
-const {
-  readFromFile,
-  writeToFile,
-  readAndAppend,
-} = require ('../helpers/fsUtils');
-
-//Get request for notes
-
-// Add code which opens uo the file and writes it into the array,
-// exercises at the back end of the week. Append, read, write...
-// 24 feedback.js example fsUtils examples.
-router.get('/notes', (req, res) => { save ()
-      .then((notes) => { return res.json(notes); })
-      .catch((err => res.status(500).json(err)));
-      readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
-      .then((json) => {
-        const result = json.filter((note) => note.note_title === note);
-        return result.length > 0
-          ? res.json(result)
-          : res.json('No note');
-      });
-});
-
-router.post('/notes', (req, res) => { save
-    .addNote(req.body)
-    .then((note) => res.json(note))
-    .catch((err) => res.status(500).json(err));
-
-  const { title, text } = req.body;
-
-  if (req.body) {
-    const newNote = {
-      title,
-      text
-    };
-
-    readAndAppend(newNote, './db/db.json');
-    res.json(`Note added successfully 🚀`);
-  } else {
-    res.error('Error in adding note');
-  }
-});
-
-//Delete notes
-router.delete('/notes/:id', (req, res) => { save
-    .removeNote(req.params.id)
-      .then(() => res.json({ ok : true })
-      .catch((err => res.status(500).json(err))
-    ));
-});
-
-//Export the module
-module.exports = router;*/
